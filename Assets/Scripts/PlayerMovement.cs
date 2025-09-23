c65e2d027ad146e0f3b52e29ed37309f372c628b
mikexql <98818517+mikexql@users.noreply.github.com>
2025-09-23 12:15:49 +0800

    initial commit finishes lab 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,119 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float maxSpeed = 20;
+    public float upSpeed = 10;
+    private bool onGroundState = true;
+
+    private Rigidbody2D marioBody;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+    public TextMeshProUGUI scoreText;
+    public GameObject enemies;
+    public JumpOverGoomba jumpOverGoomba;
+    public GameObject gameOverCanvas;
+    public GameObject inGameCanvas;
+    public TextMeshProUGUI finalScoreText;
+    // other methods
+
+    public void RestartButtonCallback(int input)
+    {
+        Debug.Log("Restart!");
+        // reset everything
+        ResetGame();
+        // resume time
+        gameOverCanvas.SetActive(false);
+        inGameCanvas.SetActive(true);
+        Time.timeScale = 1.0f;
+    }
+
+    private void ResetGame()
+    {
+        // reset position
+        marioBody.transform.position = new Vector3(-1f, -0.056f, 0.0f);
+        // reset sprite direction
+        faceRightState = true;
+        marioSprite.flipX = false;
+        // reset score
+        scoreText.text = "Score: 0";
+        // reset Goomba
+        foreach (Transform eachChild in enemies.transform)
+        {
+            eachChild.transform.localPosition = eachChild.GetComponent<EnemyMovement>().startPosition;
+        }
+        jumpOverGoomba.score = 0;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a") && faceRightState)
+        {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with goomba!");
+            Time.timeScale = 0.0f;
+            gameOverCanvas.SetActive(true);
+            inGameCanvas.SetActive(false);
+            finalScoreText.text = "Score: " + jumpOverGoomba.score.ToString();
+        }
+    }
+
+    // FixedUpdate is called 50 times a second
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.linearVelocity.magnitude < maxSpeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+        // stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            // stop
+            marioBody.linearVelocity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+    }
+}
