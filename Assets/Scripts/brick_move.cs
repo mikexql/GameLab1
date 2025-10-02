3f1566b6e7d3b4bb152290c0c2237f277e159fac
EMKwasam <samuelkwa1@gmail.com>
2025-10-03 00:53:28 +0800

    Updated brick_move.cs to ensure that the animation only plays when being hit from below

--- Assets/Scripts/brick_move.cs
+++ Assets/Scripts/brick_move.cs
@@ -14,8 +14,19 @@ public class brick_move : MonoBehaviour
         Debug.Log("Collided with " + col.collider.name);
         if (col.collider.CompareTag("Player"))
         {
-            blockAnimator.SetTrigger("hit");
-            Debug.Log("Brick hit animation triggered");
+            // Check if player is hitting from below
+            Vector2 hitDirection = col.contacts[0].point - (Vector2)transform.position;
+            
+            // If the hit point is below the block's center, player is hitting from below
+            if (hitDirection.y < 0)
+            {
+                blockAnimator.SetTrigger("hit");
+                Debug.Log("Brick hit animation triggered - hit from below");
+            }
+            else
+            {
+                Debug.Log("Brick hit from above/side - no animation");
+            }
         }
     }
 }
