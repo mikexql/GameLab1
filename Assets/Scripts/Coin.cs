7a5d0cd6ca6eda4bb2f7e1070ef6c4961613248a
EMKwasam <samuelkwa1@gmail.com>
2025-10-06 17:37:27 +0800

    Merged Coin.cs and CoinTrigger.cs

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -97,5 +97,26 @@ public class Coin : MonoBehaviour
         }
     }
 
-
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Collided with coin!");
+            
+            // Check if player is hitting from below (same logic as brick_move.cs)
+            Vector2 hitDirection = (Vector2)other.transform.position - (Vector2)transform.position;
+            
+            // If the player is below the coin's center, player is hitting from below
+            if (hitDirection.y < 0)
+            {
+                // Player is hitting coin from below
+                coin.collected = true;
+                Debug.Log("Coin collected - hit from below");
+            }
+            else
+            {
+                Debug.Log("Coin hit from above/side - no collection");
+            }
+        }
+    }
 }
